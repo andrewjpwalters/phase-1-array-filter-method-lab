13d323c0e9f1b17a920c6df242816c1f67178550
Andrew Walters <andrewjpwalters@gmail.com>
2022-03-28 23:12:01 -0500

    commit tree c07f1c923b9201f3b9b60bc377c3dbe74c549b42

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(match => match.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(item => item[string.length - 1] === string[string.length - 1])
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(item => item.name === string)
+}
